fc182ab8637f5ac60bea091e585fd00b4667191c
shristipaudel22 <128756679+shristipaudel22@users.noreply.github.com>
2025-09-29 20:19:20 -0700

    Add files via upload
    Final working code for project

--- config.py
+++ config.py
@@ -2,9 +2,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./braindump.db"
-    SUPERMEMORY_API_KEY: str = ""
-    LLM_MODEL: str = "gemma3:1b"
+    SUPERMEMORY_API_KEY: str ="sm_JuRF4dhaRY4p6LYRYR6D7t_GvyHEDeffXcvHTjZJPFxCfOjkGjHotWVGaUVAkMOxRnLsQyvgyiygSZzVphSqBbk"
+    LLM_MODEL: str = "llama3.1:8b"          
     APP_CORS_ORIGINS: str = "*"
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings()
